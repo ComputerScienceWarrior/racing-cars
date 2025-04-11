2bbfc0b42fb810c930415418d72d9fdff28ed1e2
ComputerScienceWarrior <computersciencewarrior@gmail.com>
2025-04-11 09:37:20 -0700

    adds logic for index, show, edit, update and car_params

--- app/controllers/cars_controller.rb
+++ app/controllers/cars_controller.rb
@@ -1,6 +1,6 @@
 class CarsController < ApplicationController
     def index
-
+        @cars = Car.all
     end
 
     def show
@@ -8,23 +8,34 @@ class CarsController < ApplicationController
     end
 
     def new
-
+        @car = Car.new
     end
 
     def create
-
+        car = Car.new(car_params)
+        if car.save
+            redirect_to car_path car
+        else
+            render :new
+        end
     end
 
     def edit
-
+        @car = Car.find(params[:id])
     end
 
     def update
-        
+        @car = Car.update(car_params)
+
+        if @car.save
+            redirect_to car_path @car
+        else
+            render :edit
+        end
     end
 
     def destroy
-
+        # logic for deleting a car
     end
 
     private
