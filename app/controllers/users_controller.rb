6d484478aad4a18359447d3f557e3b7ee7c93d34
ComputerScienceWarrior <computersciencewarrior@gmail.com>
2025-04-11 09:49:54 -0700

    adds logic for actions stubs

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,23 +8,29 @@ class UsersController < ApplicationController
     end
 
     def new
-
+        @user = User.new
     end
 
     def create
-
+        user = User.new(user_params)
     end
 
     def edit
-
+        @user = User.find(params[:id])
     end
 
     def update
-        
+        @user = User.update(user_params)
+
+        if @user.save
+            redirect_to
+        else
+            render :edit
+        end
     end
 
     def destroy
-
+        # adds logic to delete a user account
     end
 
     private
