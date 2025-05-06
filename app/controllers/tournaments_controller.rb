7f38a239c424da68e0c3c17c7720ff8ade946a61
ComputerScienceWarrior <computersciencewarrior@gmail.com>
2025-05-06 16:10:43 -0700

    creates actions for rournaments controller

--- app/controllers/tournaments_controller.rb
+++ app/controllers/tournaments_controller.rb
@@ -1,2 +1,48 @@
 class TournamentsController < ApplicationController
+    def index
+        @tournaments = Tournament.all
+    end
+
+    def show
+        @tournament = Tournament.find(params[:id])
+    end
+
+    def new
+        @tournament = Tournament.new
+    end
+
+    def create
+        tournament = Tournament.new(tournament_params)
+
+        if tournament
+            redirect_to tournament_path tournament
+        else
+            render :new
+        end
+    end
+
+    def edit
+        @tournament = Tournament.find(params[:tournament_id])
+    end
+
+    def update
+        tournament = Tournament.update(tournament_params)
+        if tournament.save
+            redirect_to tournament_path tournament
+        else
+            render :edit
+        end
+    end
+
+    def destroy
+        # logic to destroy a tournament
+    end
+
+    private
+
+
+
+    def tournament_params
+        params.require(:tournament).permit(:name, :size, :tournament_type, :season)
+    end
 end
