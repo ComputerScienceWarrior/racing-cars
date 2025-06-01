60e3c1fe74e138913d8fa06b3d92d3865dbdf7c8
ComputerScienceWarrior <computersciencewarrior@gmail.com>
2025-06-01 15:15:46 -0700

    simplify routing

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,5 @@ Rails.application.routes.draw do
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
 
-  get '/users/:id', to: 'users#show'
-  get '/users/new', to: 'users#new'
-  post '/users', to: 'users#create'
+  resources :users
 end
