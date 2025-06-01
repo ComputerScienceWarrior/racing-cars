cbee27a8a37a27301335a6a81af5c278bd7479c2
ComputerScienceWarrior <computersciencewarrior@gmail.com>
2025-06-01 15:17:55 -0700

    redirect to user 'show' page instead of home page

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,7 +7,7 @@ class SessionsController < ApplicationController
 
     if user&.authenticate(params[:password])
       session[:user_id] = user.id
-      redirect_to root_path, notice: "Logged in successfully!"
+      redirect_to user_path user, notice: "Logged in successfully!"
     else
       flash.now[:alert] = "Invalid email or password"
       render :new, status: :unprocessable_entity
