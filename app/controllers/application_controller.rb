9a0bb40ac6d8823e803f742b8baa24dd4ec580f4
ComputerScienceWarrior <computersciencewarrior@gmail.com>
2025-06-01 08:33:36 -0700

    adds helper methods

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::Base
+    helper_method :current_employee, :logged_in?
+
+    def current_employee
+        @current_employee ||= Employee.find_by(id: session[:employee_id]) if session[:employee_id]
+    end
+
+    def logged_in?
+        current_employee.present?
+    end
 end
